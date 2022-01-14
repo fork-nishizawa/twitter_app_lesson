65a2e8a88dd0a5e5db6ecddf2b89764db7651b82
yoh.nishizawa <nishizawa@fork.co.jp>
2022-01-14 19:46:44 +0900

    Implement display comments

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,9 +18,59 @@ interface PROPS {
   username: string
 }
 
+interface COMMENT {
+  id: string
+  avatar: string
+  text: string
+  timestamp: any
+  username: string
+}
+
+const useStyle = makeStyles((theme) => ({
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    marginRight: theme.spacing(1),
+  },
+}))
+
 const Post: React.FC<PROPS> = (props) => {
-  const [ comment, setComment ] = useState('')
+  const classes = useStyle()
   const user = useSelector(selectUser)
+  const [ comment, setComment ] = useState('')
+  const [ comments, setComments ] = useState<COMMENT[]>([
+    {
+      id: '',
+      avatar: '',
+      text: '',
+      timestamp: null,
+      username: ''
+    },
+  ])
+  const [ openComments, setOpenComments ] = useState(false)
+
+  useEffect(() => {
+    const unSub = db
+      .collection('posts')
+      .doc(props.postId)
+      .collection('comments')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().user
+          }))
+        )
+      })
+    return () => {
+      unSub()
+    }
+  }, [props.postId])
+
   const newComment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     db.collection('posts')
@@ -60,28 +110,53 @@ const Post: React.FC<PROPS> = (props) => {
             </div>
           )
         }
-        <form onSubmit={newComment}>
-          <div className={styles.post_form}>
-            <input
-              className={styles.post_input}
-              type="text"
-              placeholder="Type new comment ..."
-              value={comment}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setComment(e.target.value)
-              }}
-            />
-            <button
-              disabled={!comment}
-              className={
-                comment ? styles.post_button : styles.post_buttonDisable
+
+        <MessageIcon
+          className={styles.post_commentIcon}
+          onClick={() => {setOpenComments(!openComments)}}
+        />
+
+        {
+          openComments && (
+            <>
+              {
+                comments.map((com) => (
+                  <div key={com.id} className={styles.post_comment}>
+                    <Avatar src={com.avatar} className={classes.small} />
+                    <span className={styles.post_commentUser}>@{com.username}</span>
+                    <span className={styles.post_commentText}>{com.text}</span>
+                    <span className={styles.post_headerTime}>
+                      {new Date(com.timestamp?.toDate()).toLocaleString()}
+                    </span>
+                  </div>
+                ))
               }
-              type="submit"
-            >
-              <SendIcon className={styles.post_sendIcon} />
-            </button>
-          </div>
-        </form>
+
+              <form onSubmit={newComment}>
+                <div className={styles.post_form}>
+                  <input
+                    className={styles.post_input}
+                    type="text"
+                    placeholder="Type new comment ..."
+                    value={comment}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setComment(e.target.value)
+                    }}
+                  />
+                  <button
+                    disabled={!comment}
+                    className={
+                      comment ? styles.post_button : styles.post_buttonDisable
+                    }
+                    type="submit"
+                  >
+                    <SendIcon className={styles.post_sendIcon} />
+                  </button>
+                </div>
+              </form>
+            </>
+          )
+        }
       </div>
     </div>
   )
